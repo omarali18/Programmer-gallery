eb7e6f81f8d607229f39c25b2cb7d5baa1512cad
omarali18 <omarali1814000@gmail.com>
2021-12-04 16:32:55 +0600

    change remote test

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,7 @@ const About = () => {
     return (
         <div>
             <Header></Header>
-            <h2 className="about-text">See About our Programmers success details!!!  <i className="fas fa-meh-rolling-eyes text-danger"></i> <i className="fas fa-meh-rolling-eyes text-danger"></i></h2>
+            <h2 className="about-text">See About our Programmers success details!!! edit  <i className="fas fa-meh-rolling-eyes text-danger"></i> <i className="fas fa-meh-rolling-eyes text-danger"></i></h2>
             <Footer></Footer>
         </div>
     );
